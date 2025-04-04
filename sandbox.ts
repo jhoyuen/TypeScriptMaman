df92762dff0bde1b8fac83c7b2f96a9d7e5f869f
Zenyno <jonathan.klcflam@gmail.com>
2025-04-05 10:34:49 +1100

    JTM-3: Type Basics

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,19 @@
-const character = 'Goku';
+let character = 'Goku';
+let age = 30;
+let isSuperSaiyan = true;
 
-console.log(character);
+//character = 20; // Error: Type 'number' is not assignable to type 'string'
+character = 'Yamcha'; 
 
-const inputs = document.querySelectorAll('input');
+//age = 'thirty'; // Error: Type 'string' is not assignable to type 'number'
+age = 35;
 
-inputs.forEach(input => {
-  console.log(input);
-});
+//isSuperSaiyan = 1; // Error: Type 'number' is not assignable to type 'boolean'
+isSuperSaiyan = false;
+
+const circ = (diameter: number) => {
+  return Math.PI * diameter;
+}
+
+//console.log(circ('hello')); // Error: Argument of type 'string' is not assignable to parameter of type 'number'
+console.log(circ(5.5)); // 17.27875959474386
