b39d69bfe252c838493e867b8d3791b3caab5b6d
jhoyuen <jon_lg83@yahoo.com>
2025-04-15 15:16:47 +1000

    JTM-8: Function Basics (#8)
    Co-authored-by: Zenyno <jonathan.klcflam@gmail.com>

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world Maman!');
+let greet: Function;
+
+greet = () => { 
+    console.log("Hello, world!");
+}
+const add = (a: number, b: number, c?: number | string, d: number | string = 10) => {
+    console.log(a + b);
+    console.log(c); // from "optional" c? parameter
+    console.log(d); // from "default" d: number = 10 parameter
+}
+add(1, 2);
+
+const minus = (a: number, b: number): number => {
+    return a - b;
+}
+let result = minus(10, 5);
