916b128f2d7f541c5019c3e25414d23c985920b4
jhoyuen <jon_lg83@yahoo.com>
2025-04-22 10:44:24 +1000

    JTM-15: Interfaces (#15)
    Co-authored-by: Zenyno <jonathan.klcflam@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,44 @@
+// Interfaces
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'Mario',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+console.log(me);
+
+let someone: IsPerson;
+someone = {
+    name: 'Luigi',
+    age: 35,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+console.log(someone);
+
+const greetPerson = (person: IsPerson) => {
+    console.log('Hello', person.name);
+}
+greetPerson(me);
+greetPerson(someone);
+
 import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('Mario', 'work on the mario website', 250);
