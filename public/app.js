c1335c50ec5c40cdb82a6883a3bc7e80909bf4df
Zenyno <jonathan.klcflam@gmail.com>
2025-04-24 11:30:15 +1000

    JTM-17: Rendering an HTML template

--- public/app.js
+++ public/app.js
@@ -1,19 +1,15 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
-let docOne;
-let docTwo;
-docOne = new Invoice('Yoshi', 'web work', 250);
-docTwo = new Payment('Mario', 'plumbing work', 300);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-console.log(docs);
 const form = document.querySelector('.new-item-form');
 //inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -23,6 +19,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
-    console.log(doc.format());
+    list.render(doc, type.value, 'end');
 });
